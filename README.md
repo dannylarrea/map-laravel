7f4f80e2a8b98ede0a4b8e8f1429b366311dc578
dannylarrea <danny.larrea.argudo@gmail.com>
2022-03-15 10:21:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Descarga del proyecto:
+
+  1. clonar el repositorio (menos la persona quien creo el repo)
+  
+  2. instalar las dependencias que faltan mediante el comando `composer install`
+
+     > puede que, por problemas de compatibilidad, nos pida correr el comando`composer update`
+  
+  3. crear el fichero `.env` en el directorio principal del proyecto con el contenido pertinente
+
+     > se puede utilizar el fichero `.env.example` para generar el nuevo `.env`
+  
+  4. finalmente, Laravel puede pedir que se ejecute el comando `php artisan key:generate` para generar una nueva variable de entorno APP_KEY
+  
+  5. crear la base de datos mediante migraciones o manualmente
---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Map;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\DB;
+
+
+class MapController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $localizaciones=DB::select("SELECT * FROM tbl_churreria");
+        $categorias=DB::select("SELECT DISTINCT categoria FROM tbl_churreria");
+        //return view('books', compact('books'));
+        return view('welcome', ['localizaciones' => $localizaciones, 'categorias' => $categorias]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Map $map)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Map $map)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Map $map)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Map $map)
+    {
+        //
+    }
+}
